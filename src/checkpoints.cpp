5804fa957105d4e046726ee9c31672aee73822c4
Albert <aws.htc@gmail.com>
2016-01-09 15:29:29 -0800

    checkpoints updated

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -23,8 +23,11 @@ namespace Checkpoints
     // + Contains no strange transactions
     //
     static MapCheckpoints mapCheckpoints =
-        boost::assign::map_list_of // Yo dawg, this is the secret. Checkpoint 0 hash == Genesis block hash.
+        boost::assign::map_list_of // Checkpoint 0 hash == Genesis block hash.
         (         0, uint256("0d3d48ccf19ff662df9cf1b9660831808d01bde8708330feffd3646238d9b58f"))
+        (       100, uint256("3756192e109910ba4496da6e518160b809005d88d6fc2cc95babe891d9da5567"))
+        (       200, uint256("6617b547312a8e43b56b904ac799cceda01182ccea1aa32e025161af567b954e"))
+        (       300, uint256("fc95751a78baf9b0ba258b96f2c630eba90a600534e898200e323e21854fe911"))
         ;
 
     bool CheckBlock(int nHeight, const uint256& hash)
